3e133dc044907157c1b9ab1ba7f1c102a979c1a1
yeeun <kye9702@naver.com>
2021-11-23 16:25:01 +0900

    [프로젝트 생성]

---
+++ movieCafeWebApp/src/main/java/com/example/biz/MovieCafeWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCafeWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCafeWebAppApplication.class, args);
+	}
+
+}
