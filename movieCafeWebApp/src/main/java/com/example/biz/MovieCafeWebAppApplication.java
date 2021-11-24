a9a67f5843a86e645e4d9feb07d85222059f1e84
yeeun <kye9702@naver.com>
2021-11-24 14:14:17 +0900

    [프로젝트 생성]

--- movieCafeWebApp/src/main/java/com/example/biz/MovieCafeWebAppApplication.java
+++ movieCafeWebApp/src/main/java/com/example/biz/MovieCafeWebAppApplication.java
@@ -2,8 +2,10 @@ package com.example.biz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example"})
 public class MovieCafeWebAppApplication {
 
 	public static void main(String[] args) {
