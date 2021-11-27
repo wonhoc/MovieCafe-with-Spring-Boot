fc30dfb82adc40ae92374db33128c761dadf784d
bys <avera0128@gmil.com>
2021-11-27 17:54:33 +0900

    [기능추가]

--- movieCafeWebApp/src/main/java/com/example/member/controller/UserInfoController.java
+++ movieCafeWebApp/src/main/java/com/example/member/controller/UserInfoController.java
@@ -3,14 +3,17 @@ package com.example.member.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.mybatis.spring.SqlSessionTemplate;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.member.domain.UserInfoVo;
 import com.example.member.service.UserService;
 
 @Controller
@@ -18,22 +21,41 @@ public class UserInfoController {
 	@Autowired
 	private UserService userService;
 	
+	@GetMapping("/")
+	public String home() {
+		return "views/main";
+	}
 	
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public void loginController(@RequestParam(value = "userId") String userId, 
-								@RequestParam (value="userPwd") String userPwd) {
+//	@RequestMapping("/reqlogin")
+//	public String reqLogin() {
+//		return "/member/loginUserForm";
+//	}
+
+	@RequestMapping(value = "/requestlogin", method = RequestMethod.POST)
+	public String loginController(@RequestParam(value = "userId") String userId,
+			@RequestParam(value = "userPwd") String userPwd, HttpServletRequest req) {
 		Map<String, String> map = new HashMap<String, String>();
-		
+		String url = "";
 		map.put("userId", userId);
 		map.put("userPwd", userPwd);
-		
+
 		int isCheckUser = this.userService.isCheckUserCount(map);
 		System.out.println(isCheckUser);
 		if (isCheckUser == 1) {
-				System.out.println(userService.uploadUserInfo(userId).toString());
+			HttpSession session = req.getSession();
+			UserInfoVo user = userService.uploadUserInfo(userId);
+			session.setAttribute("userInfo", user);
+			// 로그인한 상태로 메인페이지
+			url = "/views/main";
 			
-			}
+			UserInfoVo user1 = (UserInfoVo) session.getAttribute("userInfo");
+			System.out.println("세션 유저 정보 : " + user1.toString());
+			
+		} else {
+			// 리다이렉트로 로그인 페이지 다시
+			url = "/redirect:/views/main";
 		}
-	
+		return url;
+	}
 
 }
