446b13ced4d7b61d919f3b2316952024d9ef05ef
subin <tnqls1359@naver.com>
2021-11-29 19:29:40 +0900

    [기능 추가] 게시판

--- movieCafeWebApp/src/main/java/com/example/board/dao/BoardDao.java
+++ movieCafeWebApp/src/main/java/com/example/board/dao/BoardDao.java
@@ -7,22 +7,16 @@ import com.example.board.vo.BoardVO;
 
 public interface BoardDao {
 
-	List<BoardVO> selectList(int cateNo);
-	
-	
-	void deleteBoard(int boardNo);
-	
-	void reportBoardDao(int bordNo);
-
-	void insertBoard(BoardVO board);
-	void insertBoardFile(BoardFileVO file);
-	void insertBoardNewMem(BoardVO board);
-	void insertBoardTip(BoardVO board);
-	void updateBoard(BoardVO board);
-	void updateBoardNewMem(BoardVO board);
-	BoardVO selectBoard(int boardNo);
-	int lastId();
-	List<BoardVO> selectRecomRevList();
-	List<BoardVO> selectNoticeRevList();
+   List<BoardVO> selectList(int cateNo);
+   BoardVO selectBoard(int boardNo);
+   void insertBoard(BoardVO board);
+   void insertBoardFile(BoardFileVO file);
+   void insertBoardTip(BoardVO board);
+   void updateBoard(BoardVO board);
+   void deleteBoard(int boardNo);   
+   void reportBoardDao(int bordNo);   
+   int lastId();
+   List<BoardVO> selectRecomRevList();
+   List<BoardVO> selectNoticeRevList();
 
 }
