60d5b12ffad2b3203f2ecdc4ffcd5d25b387f5be
subin <tnqls1359@naver.com>
2021-11-29 20:05:07 +0900

    [기능추가]

--- movieCafeWebApp/src/main/java/com/example/board/service/BoardService.java
+++ movieCafeWebApp/src/main/java/com/example/board/service/BoardService.java
@@ -6,15 +6,15 @@ import com.example.board.vo.BoardVO;
 
 public interface BoardService {
 
-   List<BoardVO> readAllByCateNo(int cateNo);   
-   BoardVO readOne(int boardNo);
-   void createBoard(BoardVO board);
-   void createTipBoard(BoardVO board);
-   void modifyBoard(BoardVO board);
-   void removeBoard(int boardNo);
-   void reportBoard(int boardNo);
+	List<BoardVO> readAllByCateNo(int cateNo);	
+	BoardVO readOne(int boardNo);
+	void createBoard(BoardVO board);
+	void createTipBoard(BoardVO board);
+	void modifyBoard(BoardVO board);
+	void removeBoard(int boardNo);
+	void reportBoard(int boardNo);
 
-   List<BoardVO> readRecomRevList();
-   List<BoardVO> readNoticeRevList();
+	List<BoardVO> readRecomRevList();
+	List<BoardVO> readNoticeRevList();
 
 }
