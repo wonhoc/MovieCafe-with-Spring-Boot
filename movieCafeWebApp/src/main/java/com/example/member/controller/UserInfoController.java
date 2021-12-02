88e0815504623bd8377ded5454142978ebb5709e
bys <avera0128@gmil.com>
2021-12-02 21:17:59 +0900

    [기능추가] 로그인 실패시 이동페이지 추가

--- movieCafeWebApp/src/main/java/com/example/member/controller/UserInfoController.java
+++ movieCafeWebApp/src/main/java/com/example/member/controller/UserInfoController.java
@@ -27,7 +27,7 @@ import com.example.util.FileUploadService;
 @Controller
 public class UserInfoController {
 	@Autowired
-	private UserService userService;
+	private UserService userService; 
 	
 	@Autowired
     private FileUploadService fileUploadService;
@@ -38,30 +38,30 @@ public class UserInfoController {
 		return "/views/member/joinUserForm";
 	}
 	// 로그인
-	@PostMapping("/requestlogin")
+	@PostMapping("/requestlogin") 
 	public String loginController(
 			@RequestParam(value = "userId") String userId,
-			@RequestParam(value = "userPwd") String userPwd, 
+			@RequestParam(value = "userPwd") String userPwd,
 			HttpSession session, Model model){
 		Map<String, String> map = new HashMap<String, String>();
-		map.put("userId", userId);
+		map.put("userId", userId); 
 		map.put("userPwd", userPwd);
 
 		int isCheckUser = this.userService.isCheckUserCount(map);
 		
-		System.out.println("isCheckUser = " + isCheckUser);
+		System.out.println("isCheckUser = " + isCheckUser);  
 		
 		//로그인에 성공했을 경우
-		if (isCheckUser == 1) {
-			// 세션 유지 시간 : 30분
-			session.setMaxInactiveInterval(1800);
+		if (isCheckUser == 1) { 
+			// 세션 유지 시간 : 30분 
+			session.setMaxInactiveInterval(60*30); 
 			UserInfoVo user = userService.uploadUserInfo(userId);
 			System.out.println("안녕하세요 : "+user.toString());
 			// 세션에 가져온 유저정보 등록
 			session.setAttribute("userInfo", user);
 			System.out.println(user + "000000000000000000");
 			return "redirect:/main";
-		} else {
+		} else { 
 			// 리다이렉트로 로그인 페이지 다시
 			return "redirect:/loginFail";
 		}
